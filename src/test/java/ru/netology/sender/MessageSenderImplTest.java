e17a0bd95f9f0c4b869e6df4e12b01ae42b2a51d
Yurii Parkhomenko <parkhomenko.yurij@gmail.com>
2022-05-16 20:30:44 +0300

    created MessageSenderImplTest

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -1,11 +1,17 @@
 package ru.netology.sender;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoServiceImpl;
+import ru.netology.i18n.LocalizationServiceImpl;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 
 
@@ -13,10 +19,22 @@ class MessageSenderImplTest {
 
     @Test
     public void sendTest() {
-        MessageSenderImpl messageSender = Mockito.mock(MessageSenderImpl.class);
-        Map<String, String> map = new HashMap<>();
-        map.put(MessageSenderImpl.IP_ADDRESS_HEADER, anyString());
-        Mockito.when(messageSender.send(map))
+
+        GeoServiceImpl geoService = Mockito.mock(GeoServiceImpl.class);
+        Mockito.when(geoService.byIp(anyString()))
+                .thenReturn(new Location(anyString(), Country.valueOf(anyString()), anyString(), anyInt()));
+
+        LocalizationServiceImpl localizationService = Mockito.mock(LocalizationServiceImpl.class);
+        Mockito.when(localizationService.locale(Country.valueOf(anyString())))
                 .thenReturn(anyString());
+
+        MessageSenderImpl messageSender = new MessageSenderImpl(geoService, localizationService);
+        Map<String, String> header = new HashMap<>();
+        header.put(MessageSenderImpl.IP_ADDRESS_HEADER, anyString());
+
+        String expected = localizationService.locale(Country.valueOf(anyString()));
+
+        Assertions.assertEquals(expected, messageSender.send(header));
+
     }
 }
