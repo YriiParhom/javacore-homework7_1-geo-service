fa64e95b84c35280d2be998f46362a6e2e1bfcfb
Yurii Parkhomenko <parkhomenko.yurij@gmail.com>
2022-05-17 19:33:43 +0300

    refactor code

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -2,6 +2,7 @@ package ru.netology.sender;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
@@ -21,18 +22,16 @@ class MessageSenderImplTest {
     public void sendTest() {
 
         GeoServiceImpl geoService = Mockito.mock(GeoServiceImpl.class);
-        Mockito.when(geoService.byIp(anyString()))
-                .thenReturn(new Location(anyString(), Country.valueOf(anyString()), anyString(), anyInt()));
+        geoService.byIp(anyString());
 
         LocalizationServiceImpl localizationService = Mockito.mock(LocalizationServiceImpl.class);
-        Mockito.when(localizationService.locale(Country.valueOf(anyString())))
-                .thenReturn(anyString());
+        localizationService.locale(Country.RUSSIA);
 
         MessageSenderImpl messageSender = new MessageSenderImpl(geoService, localizationService);
         Map<String, String> header = new HashMap<>();
         header.put(MessageSenderImpl.IP_ADDRESS_HEADER, anyString());
 
-        String expected = localizationService.locale(Country.valueOf(anyString()));
+        String expected = localizationService.locale(Country.RUSSIA);
 
         Assertions.assertEquals(expected, messageSender.send(header));
 
